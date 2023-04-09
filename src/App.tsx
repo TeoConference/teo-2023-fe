50619feace5f077b49967b6207f32698c5c5be87
leeharyung <lhr4884@naver.com>
2023-04-10 01:43:02 +0900

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <div>
-          <div className="font-bold underline bg-red-700">yes</div>
-        </div>
-      </header>
-    </div>
-  );
+  return <div>teo conference</div>
 }
 
-export default App;
+export default App
