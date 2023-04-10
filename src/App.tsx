bb7409980c76cf4542f002bb3d303a256bf4c206
halang <lhr4884@naver.com>
2023-04-10 15:09:48 +0900

    feat: add router (#2)
    * feat: add routes
    
    * feat: add card page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Card from 'pages/Card'
 
 function App() {
-  return <div>teo conference</div>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/card" element={<Card />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
