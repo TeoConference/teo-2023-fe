3de3c0340f6798f925c00c236f1213934aba36a1
leeharyung <lhr4884@naver.com>
2023-04-12 19:23:31 +0900

    feat: add screens const

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      desktop: '1024px',
+      tablet: '768px',
+    },
   },
   plugins: [],
 }
