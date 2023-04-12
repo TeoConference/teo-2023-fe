2d8dc11305ef263962feadad136229e63dc43662
halang <lhr4884@naver.com>
2023-04-12 19:24:46 +0900

    feat: add card (#3)
    * chore: add react-router-dom
    
    * style: add prettier
    
    * fix: change title
    
    * feat: add module
    
    * feat: add image
    
    * feat: add global css
    
    * feat: add card component
    
    * chore: add react-hook-form
    
    * feat: add screens const

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Card from 'pages/Card'
+
+import Form from 'pages/Form'
+import 'styles/global.css'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<div>Home</div>} />
-        <Route path="/card" element={<Card />} />
+        <Route path="/form" element={<Form />} />
       </Routes>
     </Router>
   )
