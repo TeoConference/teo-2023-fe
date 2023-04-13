bfd405228337aeca49f715a632d52ba317484979
leeharyung <lhr4884@naver.com>
2023-04-13 20:36:26 +0900

    feat: add Form

--- src/components/form/Card.tsx
+++ src/components/form/Card.tsx
@@ -2,28 +2,39 @@ import React from 'react'
 
 import { ReactComponent as QR } from 'assets/images/QR.svg'
 import animal from 'assets/images/SampleAnimal.png'
+import { FormData } from 'types'
 
-const Card = () => {
+const Card = (props: FormData) => {
+  const {
+    introduction,
+    company,
+    nickname,
+    developmentCareer,
+    hashTagTypeList,
+  } = {
+    ...props,
+  }
   return (
-    <div className="bg-orange-100 p-4 w-1/2 m-auto max-w-sm">
+    <div className="bg-orange-100  p-4 m-auto max-w-sm">
       <div className="flex flex-col relative h-52 justify-between">
         <div className="flex justify-between items-center">
           <div>
             <div className="text-lg font-bold text-orange-400">
-              시니어 프론트엔드 개발자
+              {introduction}
             </div>
             <div className="flex flex gap-1 items-center text-xs text-gray-500">
-              <span>카카오</span>/<span>14년차 개발자</span>
+              <span>{company}</span>/<span>{developmentCareer}년차</span>
             </div>
           </div>
           <QR className="w-8 h-8" />
         </div>
         <div className="absolute py-30 text-slate-800 text-3xl font-bold w-full h-full flex items-center justify-center">
-          Teo.yu
+          {nickname}
         </div>
-        <div className="flex items-center gap-2 text-sm text-slate-700">
-          <span>#Svelte</span>
-          <span>#함수형 프로그래밍</span>
+        <div className="flex items-center flex-wrap gap-2 text-sm text-slate-700">
+          {hashTagTypeList?.map((hashTag) => (
+            <span key={hashTag}>#{hashTag}</span>
+          ))}
         </div>
         <img
           src={animal}
