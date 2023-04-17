45ca484fb255eb66e23280b709b08c05ce6e7581
headring <myhakiki@gmail.com>
2023-04-18 02:28:53 +0900

    feat: 배포환경 routing 확인코드

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Form from 'pages/Form'
 import 'styles/global.css'
+import Main from './pages/Main'
 
 function App() {
   return (
     <Router basename={'/teo-2023-fe'}>
       <Routes>
-        <Route path="/" element={<div>Hommie</div>} />
+        <Route path="/" element={<Main />} />
         <Route path="/form" element={<Form />} />
         <Route path="/r1" element={<div>r1</div>} />
       </Routes>
---
+++ src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Main = () => {
+  return (
+    <div>
+      <div>Main</div>
+      <Link to="/form">폼으로 이동</Link>
+    </div>
+  )
+}
+export default Main
