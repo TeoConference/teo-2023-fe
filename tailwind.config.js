37a98f9089a99e584c53eb94062d816505ceddc7
SangHyun Kim <myhakiki@gmail.com>
2023-04-22 22:56:42 +0900

    Revert "style: 와이어프레임 구역 설정 및 커스텀 폰트 및 스타일 정의"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,7 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        Pretendard1: ['Pretendard-Thin'],
-        Pretendard2: ['Pretendard-ExtraLight'],
-        Pretendard3: ['Pretendard-Light'],
-        Pretendard4: ['Pretendard-Regular'],
-        Pretendard5: ['Pretendard-Medium'],
-        Pretendard6: ['Pretendard-SemiBold'],
-        Pretendard7: ['Pretendard-Bold'],
-        Pretendard8: ['Pretendard-ExtraBold'],
-        Pretendard8: ['Pretendard-Black'],
-      },
-    },
+    extend: {},
     screens: {
       desktop: '1024px',
       tablet: '768px',
