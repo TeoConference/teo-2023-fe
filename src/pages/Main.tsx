22851d26dbc0ffc224c48b8030a3f0d558483699
shkim <shkim@korda.im>
2023-04-22 16:02:01 +0900

    style: 폰트 설정 중

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,34 +5,34 @@ const Main = () => {
   return (
     <>
       <Header />
-      <section className="w-full max-w-[1920px] flex justify-center items-center h-[764px] bg-orange-200">
+      <section className="w-full max-w-[1920px] flex-center h-[916px] mt-16 bg-orange-200">
         TEOCON
       </section>
-      <section className="w-full h-[1264px] flex justify-center items-center desktop:bg-rose-950 text-white">
+      <section className="w-full h-[1264px] flex-center desktop:bg-rose-950 text-white">
         모두에게 열려있는 컨퍼런스
       </section>
-      <section className="w-full h-[800px] flex justify-center items-center desktop:bg-slate-500 text-orange-500">
+      <section className="w-full h-[800px] flex-center desktop:bg-slate-500 text-orange-500">
         프로그램
       </section>
-      <section className="w-full h-[1131px] flex justify-center items-center desktop:bg-emerald-400 text-white">
+      <section className="w-full h-[1131px] flex-center desktop:bg-emerald-400 text-white">
         네트워킹
       </section>
-      <section className="w-full h-[1499px] flex justify-center items-center desktop:bg-gray-950 text-white">
+      <section className="w-full h-[1499px] flex-center desktop:bg-gray-950 text-white">
         나만의 명함만들기
       </section>
-      <section className="w-full h-[630px] flex justify-center items-center desktop:bg-emerald-400 text-white">
+      <section className="w-full h-[630px] flex-center desktop:bg-emerald-400 text-white">
         후원사 모집
       </section>
-      <section className="w-full h-[1029px] flex justify-center items-center desktop:bg-gray-950 text-white">
+      <section className="w-full h-[1029px] flex-center desktop:bg-gray-950 text-white">
         지도
       </section>
-      <section className="w-full h-[1738px] flex justify-center items-center desktop:bg-rose-950 text-white">
+      <section className="w-full h-[1738px] flex-center desktop:bg-rose-950 text-white">
         안내사항 및 자주 묻는 질문
       </section>
-      <section className="w-full h-[888px] flex justify-center items-center desktop:bg-orange-200 text-white">
+      <section className="w-full h-[888px] flex-center desktop:bg-orange-200 text-white">
         스테프 사진
       </section>
-      <footer className="w-full h-[310px] flex justify-center items-center desktop:bg-emerald-400 text-white">
+      <footer className="w-full h-[310px] flex-center desktop:bg-emerald-400 text-white">
         푸터
       </footer>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Pretendard1: ['Pretendard1'],
+        Pretendard2: ['Pretendard2'],
+        Pretendard3: ['Pretendard3'],
+        Pretendard4: ['Pretendard4'],
+        Pretendard5: ['Pretendard5'],
+        Pretendard6: ['Pretendard6'],
+        Pretendard7: ['Pretendard7'],
+        Pretendard8: ['Pretendard8'],
+        Pretendard9: ['Pretendard9'],
+      },
+    },
     screens: {
       desktop: '1024px',
       tablet: '768px',
