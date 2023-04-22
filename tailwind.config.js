2a02df6c2bfbc24e46326695594d499f2f9dd9dc
SangHyun Kim <myhakiki@gmail.com>
2023-04-22 23:27:34 +0900

    style: 와이어 프레임, 커스텀폰트, 커스텀 스타일 정립 (#8)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Pretendard1: ['Pretendard-Thin'],
+        Pretendard2: ['Pretendard-ExtraLight'],
+        Pretendard3: ['Pretendard-Light'],
+        Pretendard4: ['Pretendard-Regular'],
+        Pretendard5: ['Pretendard-Medium'],
+        Pretendard6: ['Pretendard-SemiBold'],
+        Pretendard7: ['Pretendard-Bold'],
+        Pretendard8: ['Pretendard-ExtraBold'],
+        Pretendard8: ['Pretendard-Black'],
+      },
+    },
     screens: {
       desktop: '1024px',
       tablet: '768px',
