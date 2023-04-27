85622f6d8dbfc0b68be8254902f1efa9d1e6a679
headring <myhakiki@gmail.com>
2023-04-28 00:01:33 +0900

    chore: nextconfig 안 gh-pages 설정 삭제

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  assetPrefix: isProd ? "https://teoconference.github.io/teo-2023-fe/" : "",
   reactStrictMode: true,
-  output: 'export',
 }
 
 module.exports = nextConfig
