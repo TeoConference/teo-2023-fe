f29f482797991833b1e96637cedb562585009855
leeharyung <lhr4884@naver.com>
2023-04-27 16:40:01 +0900

    fix: change div tag to section

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className="bg-black text-white h-[calc(100vh_-_4rem)]">
+    <section className="bg-black text-white h-[calc(100vh_-_4rem)]">
       <div className="p-16 flex flex-col h-full justify-end tablet:p-40">
         <h1 className="text-6xl tablet:text-7xl font-bold">TEOConf 2023</h1>
         <div className="text-xl tablet:text-3xl flex flex-col mt-10">
@@ -10,7 +10,7 @@ const Banner = () => {
           <span>23.05.20 토요일, 사람인 카페</span>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
