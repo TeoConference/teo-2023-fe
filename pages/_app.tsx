78a1aea3a1aa3dbf8396283a908a709fb8c70921
halang <lhr4884@naver.com>
2023-04-27 21:44:04 +0900

    feat: add map (#11)
    * feat: add env example
    
    * feat: add kakao map

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+declare global {
+  interface Window {
+    kakao: any
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
