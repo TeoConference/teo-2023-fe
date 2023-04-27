dcf59dc2459a880422696593ad676f13be703ec1
leeharyung <lhr4884@naver.com>
2023-04-27 21:38:33 +0900

    feat: add kakao map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,38 +1,27 @@
 import React from 'react'
+import KakaoMap from '@/components/common/KakaoMap'
 
-const Map = () => (
-  <div className="w-[100%] inline-flex flex-col items-center justify-center px-2.5 py-28 bg-gray-900">
-    <div
-      className="flex flex-col space-y-20 items-center justify-start"
-      style={{ width: 1024, height: 689 }}
-    >
-      <div
-        className="flex flex-col space-y-4 items-center justify-center"
-        style={{ width: 507, height: 111 }}
-      >
-        <div
-          className="inline-flex space-x-4 items-start justify-start"
-          style={{ width: 493, height: 56 }}
-        >
-          <p className="text-5xl font-bold leading-10 text-center text-white">
-            5월 20일
-          </p>
-          <p className="text-5xl font-bold leading-10 text-center text-green-500">
-            여기서 만나요!
+const Map = () => {
+  return (
+    <div className="w-full h-screen bg-gray-900 tablet:p-24 p-10">
+      <div className="flex flex-col h-full tablet:gap-20 gap-10 items-center justify-around tablet:w-full">
+        <div className="flex flex-col flex-wrap gap-4 tablet:gap-8 items-center justify-center">
+          <div className="inline-flex flex-wrap gap-2 tablet:gap-4 items-start justify-start text-4xl tablet:text-5xl">
+            <p className="font-bold leading-10 text-center text-white">
+              5월 20일
+            </p>
+            <p className="font-bold leading-10 text-center text-green-500">
+              여기서 만나요!
+            </p>
+          </div>
+          <p className="text-xl tablet:text-2xl font-bold leading-10 tablet:text-center text-white">
+            신촌역 3번출구에서 300M 내에 위치하고 있습니다!
           </p>
         </div>
-        <p className="text-2xl font-bold leading-10 text-center text-white">
-          신촌역 3번출구에서 300M 내에 위치하고 있습니다!
-        </p>
+        <KakaoMap latitude={37.55759668300872} longitude={126.93709078486495} />
       </div>
-      <img
-        alt=""
-        className="rounded-lg"
-        style={{ width: 1024, height: 500 }}
-        src="https://via.placeholder.com/1024x500"
-      />
     </div>
-  </div>
-)
+  )
+}
 
 export default Map
---
+++ components/common/KakaoMap.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react'
+
+interface KakaoMapProps {
+  latitude: number
+  longitude: number
+}
+
+const KakaoMap = ({ latitude, longitude }: KakaoMapProps) => {
+  useEffect(() => {
+    const mapScript = document.createElement('script')
+
+    mapScript.async = true
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP}&autoload=false`
+
+    document.head.appendChild(mapScript)
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('map')
+        const options = {
+          center: new window.kakao.maps.LatLng(latitude, longitude),
+        }
+        const map = new window.kakao.maps.Map(container, options)
+        const markerPosition = new window.kakao.maps.LatLng(latitude, longitude)
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+        })
+        marker.setMap(map)
+      })
+    }
+    mapScript.addEventListener('load', onLoadKakaoMap)
+
+    return () => mapScript.removeEventListener('load', onLoadKakaoMap)
+  }, [latitude, longitude])
+
+  return <div id="map" className="w-full h-60 tablet:h-96" />
+}
+
+export default KakaoMap
