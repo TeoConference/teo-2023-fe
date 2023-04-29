a1958428c79b380a67a06186ed94dd4266a7ab0d
headring <myhakiki@gmail.com>
2023-04-29 16:26:45 +0900

    style: Logo 작업 중

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
+import Logo from '../public/images/Logo.png'
 // import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <header className="w-full h-16 backdrop-blur-sm bg-white bg-opacity-90 flex justify-center items-center sticky top-0 text-[16px] z-50 p-4">
       <nav className="w-full max-w-[1360px] flex justify-between">
-        <div className="font-extrabold">TEOConf</div>
+        <div>
+          <Image alt="logo" src={Logo} width={110} height={19.04} />
+        </div>
         <div className="inline-flex space-x-10 items-end justify-start">
           <button type="button">프로그램</button>
           <button type="button">네트워킹</button>
