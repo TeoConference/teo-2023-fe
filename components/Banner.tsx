86e0eb06f4da849876354a7080ec3ca404ef25fd
headring <myhakiki@gmail.com>
2023-05-01 00:45:12 +0900

    style: banner font 적용

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -13,10 +13,10 @@ const Banner = () => {
           width={461.36}
           height={281}
         />
-        <h1 className="mt-auto text-6xl tablet:text-7xl font-bold">
+        <h1 className="font-Pretendard6 mt-auto text-6xl tablet:text-7xl font-bold mb-7">
           TEOConf 2023
         </h1>
-        <div className="text-xl tablet:text-3xl flex flex-col mt-10">
+        <div className="font-Pretendard6 text-xl tablet:text-5xl flex flex-col mt-10">
           <span>테오의 컨퍼런스</span>
           <span>23.05.20 토요일, 사람인 카페</span>
         </div>
