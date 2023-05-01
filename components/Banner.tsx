75ce0e456a7c4456529a43269bc088d795751ec4
leeharyung <lhr4884@naver.com>
2023-05-01 11:26:42 +0900

    design: add padding in banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -7,16 +7,16 @@ const Banner = () => {
     <section className="bg-black text-white h-[calc(100vh_-_3rem)] mobile:h-[calc(100vh_-_4rem)] flex-center">
       <div className="desktop:w-[1360px] flex flex-col h-full justify-end desktop:pb-40">
         <Image
-          className="mt-36 ml-auto"
+          className="mt-36 ml-auto px-4"
           alt="TeoBI"
           src={TeoBI}
           width={461.36}
           height={281}
         />
-        <h1 className="font-Pretendard6 mt-auto text-6xl tablet:text-7xl font-bold mb-7">
+        <h1 className="font-Pretendard6 mt-auto text-6xl tablet:text-7xl font-bold mb-7 px-4">
           TEOConf 2023
         </h1>
-        <div className="font-Pretendard6 text-xl tablet:text-5xl flex flex-col mt-10">
+        <div className="font-Pretendard6 text-xl tablet:text-5xl flex flex-col mt-10 tablet:px-4 pb-4">
           <span>테오의 컨퍼런스</span>
           <span>23.05.20 토요일, 사람인 카페</span>
         </div>
