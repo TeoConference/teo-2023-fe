68ef3b48024da1952059e21ecd91726db6565b20
SangHyun Kim <myhakiki@gmail.com>
2023-05-02 01:17:47 +0900

    Response banner (#21)
    * chore: 커스텀 css component 삭제
    
    * design: 프로그램 svg 대체
    
    * refactor: title 수정
    
    * style: 배너 적용 완료

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,12 +5,12 @@ import MenuIcon from '@mui/icons-material/Menu'
 
 export const Header = () => {
   return (
-    <header className="w-full mobile:h-16 h-12 backdrop-blur-sm bg-white bg-opacity-90 flex justify-center items-center sticky top-0 text-[16px] z-50 p-4">
-      <nav className="w-full flex justify-between">
+    <header className="w-full mobile:h-16 h-12 backdrop-blur-sm bg-white bg-opacity-90 flex-center sticky top-0 text-[16px] z-50 p-4">
+      <nav className="w-full flex justify-between tablet:mx-12 desktop:max-w-[1360px]">
         <div className="h-[19.04px]">
           <Image alt="logo" src={Logo} width={110} height={19.04} />
         </div>
-        <div className="space-x-10 items-end justify-start hidden tablet:inline-flex">
+        <div className="space-x-10 items-end justify-start hidden desktop:inline-flex">
           <button type="button">프로그램</button>
           <button type="button">네트워킹</button>
           <button type="button">명함</button>
