f54fab699e5a1c3cfef32a57783c007e2878bd6a
leeharyung <lhr4884@naver.com>
2023-05-01 20:25:04 +0900

    fix: change talkData

--- data/Talk.ts
+++ data/Talk.ts
@@ -17,15 +17,7 @@ export const talkData: TalkData[] = [
     speaker: '주니어 BE개발자',
   },
   {
-    contents: '개발자가 되기 위해\n준비해야 할 것들은 무엇일까?',
-    speaker: '개발자를 꿈꾸는 대학생',
-  },
-  {
-    contents: '사이드 프로젝트\n팀원을 구하고 싶은데..',
-    speaker: '주니어 FE개발자',
-  },
-  {
-    contents: '다른 사람들과 같이 \n이야기 할 수 있는 기회가 있으면 좋겠다',
-    speaker: '이야기를 하고싶은 개발자',
+    contents: '사이드 프로젝트 팀원을\n구하고 싶은데...',
+    speaker: 'FE개발자',
   },
 ]
