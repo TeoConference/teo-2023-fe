103c7ec4161780e6103c6b3b54f1e06b43fc4057
halang <lhr4884@naver.com>
2023-05-01 20:28:24 +0900

    feat: add responsive ui in Introduce (#18)
    * chore: add introduce image
    
    * design: add colors, fontSize const
    
    * fix: change swiperSlide to common component
    
    * fix: fix interface
    
    * fix: change talkData
    
    * fix: add responsive ui

--- components/introduce/Talk.tsx
+++ components/introduce/Talk.tsx
@@ -4,22 +4,23 @@ import clsx from 'clsx'
 interface TalkProps {
   contents: string
   speaker: string
+  Icon: React.ReactNode
   classNames?: string
 }
 
-const Talk = ({ contents, speaker, classNames }: TalkProps) => {
+const Talk = ({ contents, speaker, Icon, classNames }: TalkProps) => {
   return (
     <li className={clsx('flex flex-col gap-10 whitespace-pre', classNames)}>
-      <div className="flex flex-col relative space-y-5 items-start justify-start px-8 py-10 bg-white rounded-3xl">
-        <p className="tablet:text-xl text-lg font-bold text-gray-900">
+      <div className="flex flex-col relative space-y-5 items-start justify-start mobile:px-8 mobile:py-10 p-6 bg-white rounded-3xl">
+        <p className="mobile:text-h3 text-mobile-h2 font-bold text-gray-900">
           {contents}
         </p>
         <p className="text-base leading-normal text-center text-gray-900">
           {speaker}
         </p>
-        <div className="absolute transform bottom-[-1rem] left-[3rem] rotate-45 h-10 w-12 bg-white rounded-lg" />
+        <div className="absolute transform bottom-[-1rem] left-[3rem] rotate-45 h-10 w-10 bg-white rounded-lg" />
       </div>
-      <div className="h-32 w-32 bg-gray-300 rounded-full ml-24" />
+      {Icon}
     </li>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,32 @@ module.exports = {
         Pretendard8: ['Pretendard-ExtraBold'],
         Pretendard8: ['Pretendard-Black'],
       },
+      colors: {
+        'green-main': '#00DE4F',
+      },
+    },
+    fontSize: {
+      h1: ['3.125rem', { lineHeight: '3.5rem', fontWeight: '700' }],
+      h2: ['2.5rem', { lineHeight: '2.8rem', fontWeight: '700' }],
+      h3: ['1.625rem', { lineHeight: '2.4rem', fontWeight: '700' }],
+      'sub-h1': ['1.3rem', { lineHeight: '2.3rem', fontWeight: '700' }],
+      'sub-h2': [
+        '1rem',
+        { lineHeight: '1.5rem', fontWeight: '700', fontWeight: '700' },
+      ],
+      body1: ['1.3rem', { lineHeight: '2.3rem', fontWeight: '400' }],
+      body2: ['1rem', { lineHeight: '1.5rem', fontWeight: '400' }],
+      body3: ['0.8rem', { lineHeight: '1.3rem', fontWeight: '400' }],
+      'mobile-h1': ['2.125rem', { lineHeight: '2.75rem', fontWeight: '700' }],
+      'mobile-h2': ['1.125rem', { lineHeight: '1.75rem', fontWeight: '700' }],
+      'mobile-body2': [
+        '0.875rem',
+        { lineHeight: '1.375rem', fontWeight: '400' },
+      ],
+      'mobile-sub-h2': [
+        '0.875rem',
+        { lineHeight: '1.375rem', fontWeight: '700' },
+      ],
     },
     screens: {
       'desktop-xl': '1920px',
