e8516c632cacf9facda5f0e569004196964b6a68
SangHyun Kim <myhakiki@gmail.com>
2023-05-03 02:25:42 +0900

    chore: mui deploy test (#24)
    * chore: mui deploy test
    
    * chore: mui css test
    
    * chore: nextconfig rollback

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
