276815a61081a0ae6aed5f1427d98a5f7e1ef884
SangHyun Kim <myhakiki@gmail.com>
2023-05-03 01:28:03 +0900

    style: 반응형 배너 적용 (#23)

--- components/Sponsor.tsx
+++ components/Sponsor.tsx
@@ -1,22 +1,30 @@
+import Image from 'next/image'
 import React from 'react'
+import JumpitLogo from '../public/images/JumpitLogo.png'
 
 const Sponsor = () => (
-  <section className="flex flex-col items-center justify-center px-2.5 py-[120px] bg-gray-100">
-    <div className="flex flex-col space-y-9 items-center justify-start">
-      <div className="flex flex-col space-y-12 items-center justify-start">
-        <div className="flex flex-col space-y-4 items-center justify-start">
-          <p className="text-5xl font-bold leading-10 text-center">
+  <section className="flex flex-col items-center justify-center h-[16.195rem] tablet:h-[26.4375rem] desktop:h-[28.9375rem] bg-gray-100">
+    <div className="flex-center flex-col">
+      <div className="flex-center flex-col">
+        <div className="flex-center flex-col">
+          <p className="font-Pretendard font-bold text-[2.125rem] tablet:text-[3.125rem] leading-10 text-center">
             후원사 소개
           </p>
-          <p className="text-2xl font-bold leading-10 text-center">
+          <p className="font-Pretendard font-medium text-[1.125rem] leading-10 text-center tablet:hidden">
+            테오의 컨퍼런스 1기는 <br /> 사림인의 &apos;점핏&apos;에서
+            후원합니다.
+          </p>
+          <p className="hidden tablet:block font-Pretendard font-bold text-[1.625rem] mt-8">
             테오의 컨퍼런스 1기는 사림인의 &apos;점핏&apos;에서 후원합니다.
           </p>
         </div>
       </div>
-      <div className="inline-flex items-center justify-center w-64 h-16 px-6 py-2.5 bg-gray-900 rounded-full">
-        <p className="text-base font-medium leading-9 text-center text-white">
-          자세히 알아보기
-        </p>
+      <div className="flex-center h-fit">
+        <Image
+          alt="JumpitLogo"
+          src={JumpitLogo}
+          className="w-[140px] h-[59.13px] tablet:mt-[3.75rem] tablet:w-[206px] tablet:h-[87px]"
+        />
       </div>
     </div>
   </section>
