680126bdd4d31606d8f64d2b12deb2bf47963af3
halang <lhr4884@naver.com>
2023-05-03 19:39:28 +0900

    feat: add responsive ui (#27)
    * feat: add bg image
    
    * feat: add name card image
    
    * design: fix padding in networking
    
    * feat: add responsive ui in name card

--- components/NameCard.tsx
+++ components/NameCard.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
 
 const NameCard = () => (
-  <section className="flex flex-col space-y-16 items-center justify-start py-[180px]">
-    <div className="flex flex-col space-y-20 items-center justify-start">
-      <div className="flex flex-col space-y-12 items-center justify-start">
-        <div className="flex flex-col space-y-4 items-center justify-start">
-          <p className="text-5xl font-bold leading-10 text-center">
-            나만의 명함 만들기
-          </p>
-          <p className="text-2xl font-bold leading-10 text-center">
+  <section className="flex flex-col items-center gap-6 py-8 tablet:px-28 px-4">
+    <div className="flex flex-col space-y-8 items-center w-full">
+      <div className="flex flex-col space-y-10 items-center">
+        <div className="flex flex-col space-y-4 items-center">
+          <p className="text-mobile-h1 text-center">나만의 명함 만들기</p>
+          <p className="text-mobile-h2 text-center">
             나를 소개하는 멋진 명함을 만들어보세요!
           </p>
         </div>
       </div>
-      <div className="bg-gray-100 rounded-lg" />
+      <div className="bg-name-card bg-cover rounded-lg h-[250px] tablet:h-[280px] desktop:h-[367px] w-full" />
     </div>
-    <div className="inline-flex items-center justify-center w-1/5 h-16 px-6 py-2.5 bg-gray-900 rounded-full">
-      <p className="text-base font-medium leading-9 text-center text-white">
+    <button
+      type="button"
+      className="inline-flex items-center justify-center px-8 py-4 bg-gray-900 rounded-full"
+    >
+      <p className="text-mobile-body2 text-center text-white">
         자세히 알아보기
       </p>
-    </div>
+    </button>
   </section>
 )
 
