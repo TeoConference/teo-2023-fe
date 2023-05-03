192ae074b3a1b2f1483691cce08b3e9ea3e14d19
leeharyung <lhr4884@naver.com>
2023-05-04 00:14:03 +0900

    feat: add Notice

---
+++ components/Notice.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { NoticeData } from '@/data/FAQ'
+import FAQField from './common/FAQField'
+import type { FAQ as FAQType } from '@/types'
+
+const Notice = () => {
+  return (
+    <section>
+      <div className="flex flex-col gap-6 items-center max-w-[1024px] mx-auto px-8 py-16">
+        <div className="flex flex-col">
+          <p className="text-mobile-h1 tablet:text-h1 text-center">안내사항</p>
+        </div>
+        <div className="flex flex-col gap-2 tablet:gap-4 w-full max-w-[860px]">
+          {NoticeData.map((faq: FAQType) => (
+            <FAQField
+              key={faq.question}
+              question={faq.question}
+              answer={faq.answer}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Notice
