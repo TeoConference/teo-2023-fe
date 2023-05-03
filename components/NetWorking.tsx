c0cf3290bda2938ad10316cc01b9bbab9df0eba1
leeharyung <lhr4884@naver.com>
2023-05-03 22:46:32 +0900

    design: fix networking

--- components/NetWorking.tsx
+++ components/NetWorking.tsx
@@ -85,7 +85,7 @@ const Networking = () => (
             height="100"
           />
         </div>
-        <div className="flex flex-col space-y-4">
+        <div className="flex flex-col space-y-2 whitespace-nowrap">
           <div className="flex flex-col space-y-1.5">
             <p className="text-mobile-h2 tablet:text-h3 text-gray-900">
               함께하고 싶어요!
