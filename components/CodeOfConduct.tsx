d62504320bafc9cb1428c9f0ed84147788b3e93a
leeharyung <lhr4884@naver.com>
2023-05-04 00:13:47 +0900

    feat: add code of conduct

---
+++ components/CodeOfConduct.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import FAQField from './common/FAQField'
+import { CodeOfConductData } from '@/data/FAQ'
+import type { FAQ as FAQType } from '@/types'
+
+const CodeOfConduct = () => {
+  return (
+    <section>
+      <div className="flex flex-col gap-6 items-center max-w-[1024px] mx-auto px-8 py-16">
+        <div className="flex flex-col">
+          <p className="text-mobile-h1 tablet:text-h1 text-center">행동강령</p>
+        </div>
+        <div className="flex flex-col gap-2 tablet:gap-4 w-full max-w-[860px]">
+          {CodeOfConductData.map((faq: FAQType) => (
+            <FAQField
+              key={faq.question}
+              question={faq.question}
+              answer={faq.answer}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default CodeOfConduct
