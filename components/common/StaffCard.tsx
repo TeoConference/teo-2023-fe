ec8f83405dba5d0a9c6777df894c5c276856a2a6
halang <lhr4884@naver.com>
2023-05-04 22:23:09 +0900

    fix: fix staff card (#31)
    * fix: fix staff card
    
    * design: fix behance logo size

--- components/common/StaffCard.tsx
+++ components/common/StaffCard.tsx
@@ -30,27 +30,35 @@ const StaffCard = ({
       )}
     >
       <Image src={image} alt={`${name} 이미지`} className="w-full" />
-      <div className="flex flex-col bg-gray-900 px-4 py-3 tablet:px-5 tablet:py-4 items-start gap-1 justify-between rounded-b-xl desktop:rounded-b-2xl">
+      <a
+        href={url}
+        target="_blank"
+        className="flex flex-col bg-gray-900 px-4 py-3 tablet:px-5 tablet:py-4 items-start gap-1 justify-between rounded-b-xl desktop:rounded-b-2xl"
+      >
         <div className="flex justify-between items-center w-full">
-          <p className="text-mobile-sub-h2 tablet:text-sub-h1 text-green-main">
+          <p className="text-mobile-sub-h2 tablet:text-mobile-h2 desktop:text-sub-h1 text-green-main">
             {name}
           </p>
-          <a href={url} target="_blank">
+          <div>
             {position === 'developer' ? (
               <Image
                 src={GithubLogo}
                 alt="github 바로가기"
-                className="w-4 desktop:w-6"
+                className="w-4 tablet:w-5 desktop:w-6"
               />
             ) : (
-              <Image src={Behance} alt="behance 바로가기" className="w-4" />
+              <Image
+                src={Behance}
+                alt="behance 바로가기"
+                className="w-4 tablet:w-5 desktop:w-6"
+              />
             )}
-          </a>
+          </div>
         </div>
         <p className="hidden desktop:block text-white text-body3 mt-1">
           {description}
         </p>
-      </div>
+      </a>
     </li>
   )
 }
