d0d6d3eb724c2207391a16b529b644f085f59f7b
leeharyung <lhr4884@naver.com>
2023-05-04 23:40:09 +0900

    design: fix sns css

--- components/SNS.tsx
+++ components/SNS.tsx
@@ -6,9 +6,9 @@ import Twitter from '@/public/images/Twitter.svg'
 import Instagram from '@/public/images/Instagram.svg'
 
 const SNS = () => (
-  <div className="w-[100%] inline-flex flex-col items-center justify-center px-2.5 py-14 tablet:py-20 bg-gray-900">
+  <div className="w-[100%] inline-flex flex-col items-center justify-center px-2.5 py-14 tablet:py-20 desktop:py-26 desktop-xl:py-30 bg-gray-900">
     <div className="flex flex-col items-center max-w-[700px] mx-auto">
-      <p className="text-h3 tablet:text-h2 desktop:text-h1 text-center text-white">
+      <p className="text-h3 text-center text-white">
         테오콘 소식을 <br className="block tablet:hidden" /> 더 빨리 알고 싶다면
       </p>
       <div className="flex mt-6 tablet:mt-14 gap-4">
