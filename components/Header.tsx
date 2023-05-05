8932742767033af05e4c2312985258d96d5b23f6
SangHyun Kim <myhakiki@gmail.com>
2023-05-06 01:52:07 +0900

    privacy modal (#46)
    * feat: 개인정보 처리 모달 완료
    
    * refactor: 미사용 import 삭제
    
    * refactor: 글자 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,6 @@ export const Header = () => {
   const [currentId, setCurrentId] = useState('')
   const [isOpen, setIsOpen] = useState(false)
   useIntersectionObservation(setCurrentId, currentId)
-  console.log(currentId)
 
   const toggleSide = () => {
     setIsOpen((prev) => !prev)
