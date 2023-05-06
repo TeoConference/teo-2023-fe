700c673208046190556805092df6cbc51467f6c4
leeharyung <lhr4884@naver.com>
2023-05-06 12:51:40 +0900

    fix: add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="images/favicon/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
