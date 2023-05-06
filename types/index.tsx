a3dea8e46b0f60ef687bd3dcc7223fdaf462f40a
leeharyung <lhr4884@naver.com>
2023-05-06 12:51:19 +0900

    fix: delete form data type

--- types/index.tsx
+++ types/index.tsx
@@ -1,15 +1,3 @@
-export type FormData = {
-  introduction?: string
-  company?: string
-  developmentCareer?: number
-  nickname?: string
-  qrLink?: string
-  hashTagTypeList?: string[]
-  animalType?: string
-  colorType?: string
-  email?: string
-}
-
 export type FAQ = {
   question: string
   answer: string
