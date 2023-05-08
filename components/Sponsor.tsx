948127395ab3a936bcaba8f9913525e13139f006
teo.yu <developer.1px@gmail.com>
2023-05-08 10:32:35 +0900

    점핏 후원사 링크와 슬로건 추가

--- components/Sponsor.tsx
+++ components/Sponsor.tsx
@@ -13,23 +13,26 @@ const Sponsor = () => (
           <p className="font-Pretendard font-bold text-[2.125rem] tablet:text-[3.125rem] leading-10 text-center">
             후원사 소개
           </p>
-          <p className="font-Pretendard font-medium text-[1.125rem] leading-10 text-center tablet:hidden">
-            테오의 컨퍼런스 1기는 <br /> 사람인의 &apos;점핏&apos;에서
-            후원합니다.
+          <p className="font-Pretendard font-medium text-[1.125rem] text-center tablet:hidden">
+            테오의 컨퍼런스 1기는 <br/> 사람인의 <a className="hover:underline" target="_blank" href="https://www.jumpit.co.kr/">&apos;점핏&apos;</a>에서
+            후원합니다.<br/>
           </p>
           <p className="hidden tablet:block font-Pretendard font-bold text-[1.625rem] mt-8">
-            테오의 컨퍼런스 1기는 사람인의 &apos;점핏&apos;에서 후원합니다.
+            테오의 컨퍼런스 1기는 사람인의 <a className="hover:underline" target="_blank" href="https://www.jumpit.co.kr/">&apos;점핏&apos;</a>에서 후원합니다.<br/>
           </p>
         </div>
       </div>
-      <div className="flex-center h-fit">
-        <Image
-          alt="JumpitLogo"
-          src={JumpitLogo}
-          className="tablet:mt-[3.75rem] tablet:w-[206px] tablet:h-[87px]"
-          width={140}
-          height={59.13}
-        />
+      <div className="flex-center h-fit mt-4">
+        <a target="_blank" href="https://www.jumpit.co.kr/">
+          <Image
+            alt="JumpitLogo"
+            src={JumpitLogo}
+            className="tablet:mt-[3.75rem] tablet:w-[206px] tablet:h-[87px]"
+            width={140}
+            height={59.13}
+          />
+          <p className="font-Pretendard font-medium tablet:text-[1rem] text-[0.75rem] text-center">개발자 커리어 점프, 점핏!</p>
+        </a>
       </div>
     </div>
   </section>
