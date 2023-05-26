4bab8a9e185cb3968ed8e333b7c74b58c9581ab8
halang <lhr4884@naver.com>
2023-05-26 16:57:14 +0900

    feat: add program pdf (#56)
    * style: delete width height props
    
    * fix: delete unused image
    
    * feat: add program pdf

--- components/Program.tsx
+++ components/Program.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 import clsx from 'clsx'
 import Image from 'next/image'
 
-import QuestionMark1 from '@/public/images/QuestionMark1.svg'
-import QuestionMark2 from '@/public/images/QuestionMark2.svg'
-
 import chedar from '@/public/images/speakers/chedar.png'
 import derrick from '@/public/images/speakers/derrick.png'
 import moseung from '@/public/images/speakers/moseung.png'
 import nagnae from '@/public/images/speakers/nagnae.png'
 import parang from '@/public/images/speakers/parang.png'
 import teo from '@/public/images/speakers/teo.png'
+import SaveAltIcon from '@mui/icons-material/SaveAlt'
 
 const SPEAKERS = [
   {
@@ -20,6 +18,7 @@ const SPEAKERS = [
     subtitle:
       '테오의 스프린트 다회차 경험 + 진행까지 맡아본 경험으로 취업까지 성공하기',
     korname: '모승',
+    pdf: 'https://drive.google.com/file/d/1prp7V-azTeW6ItMdsnIQQi3GMPktqwl0/view?usp=sharing',
   },
   {
     id: 'chedar',
@@ -27,6 +26,7 @@ const SPEAKERS = [
     title: '안녕하세요, 프론트엔드 개발 인턴입니다.',
     subtitle: '인턴으로 할 수 있었던 경험 A - Z',
     korname: '체다',
+    pdf: 'https://drive.google.com/file/d/1vd32vqnrVs-9WQMX8e8Z9S3g1KnamlBq/view?usp=sharing',
   },
   {
     id: 'derrick',
@@ -35,6 +35,7 @@ const SPEAKERS = [
     subtitle:
       '희귀난치병 환자가 비대면 진료 스타트업 개발자가 되기까지 배운 것',
     korname: '데릭',
+    pdf: 'https://drive.google.com/file/d/1RTMFrXTblqiC6mRufI31OAmDYamq26JA/view?usp=sharing',
   },
   {
     id: 'parang',
@@ -42,6 +43,7 @@ const SPEAKERS = [
     title: '디자인 컴포넌트 구성하기',
     subtitle: '프로젝트에 옷장 만들기 with React',
     korname: '파랑',
+    pdf: 'https://drive.google.com/file/d/1ZvNdN9Fikz-k_FinSgF6KIuDcb1z8UbX/view?usp=sharing',
   },
   {
     id: 'nagnae',
@@ -49,6 +51,7 @@ const SPEAKERS = [
     title: '16년에 FE를 배우는 기분, 23년에 FE 개발자로 일하는 기분',
     subtitle: '프론트엔드 개발자로 일하며 느낀 현실과 이상',
     korname: '나그네',
+    pdf: 'https://drive.google.com/file/d/1epjVFYSjEowyJ3Hi86R7cvewn_n2zEWr/view?usp=sharing',
   },
   {
     id: 'teo',
@@ -56,6 +59,7 @@ const SPEAKERS = [
     title: '잘하는 개발자가 되는 그라데이션 사고법',
     subtitle: 'FE개발자 관점의 추상화된 계층에 대한 이해',
     korname: '테오',
+    pdf: 'https://drive.google.com/file/d/1mXfVjiRJ-e-i9UjpZkuxdCyptZdWvuMb/view?usp=sharing',
   },
 ]
 
@@ -75,49 +79,56 @@ const Program = () => (
           </p>
         </div>
       </div>
-      <div className="w-full grid tablet:grid-cols-2 desktop:gap-4 text-white desktop:h-fit">
+      <div className="w-full grid tablet:grid-cols-2 tablet:gap-4 text-white">
         {SPEAKERS.map((speaker, i) => (
           <div
             key={speaker.id}
             className={clsx(
-              'flex flex-col bg-black tablet:rounded-xl  tablet:p-6 tablet:m-[10px] desktop:p-6 desktop-xl:p-8 desktop-xl:w-[670px] desktop-xl:h-[200px] desktop:w-[502px] desktop:h-[210px] tablet:w-[23.375rem] tablet:h-[16.8125rem] desktop:justify-between justify-between break-keep',
+              'flex flex-col bg-black tablet:rounded-xl tablet:p-6 desktop:p-6 desktop-xl:p-8 desktop:justify-between justify-between break-keep',
               i == 0 ? 'rounded-t-lg' : i == 5 ? 'rounded-b-lg' : '',
               i < 5 ? 'px-4 pt-5 pb-0' : 'px-4 pt-5 pb-4'
             )}
           >
             <div
               className={clsx(
-                `h-full w-full flex flex-col justify-between tablet:border-none tablet:p-2`,
-                i < 5 ? 'pb-5 border-b border-white' : 'pb-3'
+                `h-full w-full flex flex-col justify-between tablet:border-none`,
+                i < 5 ? 'border-b border-white pb-5 tablet:pb-0' : ''
               )}
             >
-              <div className="flex flex-col tablet:space-y-3 desktop:h-[60%]">
-                <p className="desktop:text-[1.3rem] desktop:leading-7 desktop:font-bold tablet:text-sub-h1 text-sub-h2 tablet:flex tablet:justify-start items-start text-[#00DE4F]">
-                  <span>{speaker.title}</span>
-                </p>
-                <p className="tablet:text-body2 text-[13px] leading-5 desktop:w-[303px] desktop-xl:w-full">
+              <div className="flex flex-col tablet:space-y-3">
+                <span className="tablet:text-sub-h1 text-sub-h2 tablet:flex tablet:justify-start items-start text-green-main">
+                  {speaker.title}
+                </span>
+                <span className="tablet:text-body2 text-mobile-body2">
                   {speaker.subtitle}
-                </p>
+                </span>
               </div>
-              <div className="flex items-end justify-between desktop:items-center desktop:h-[40%] desktop-xl:relative">
-                <div>
-                  <p>
+              <div className="flex justify-between items-end mt-4 desktop:items-end">
+                <div className="flex flex-col justify-between desktop:flex-row desktop:gap-4 desktop:items-center">
+                  <div>
                     <span className="tablet:text-sub-h2 text-mobile-sub-h2">
                       {speaker.korname}{' '}
                     </span>
-                    <span className="tablet:text-[1rem] tablet:leading-6 text-[14px] leading-[22px] font-normal">
+                    <span className="text-mobile-body2 tablet:text-body2">
                       | 프론트엔드 개발자
                     </span>
-                  </p>
+                  </div>
+                  <button className="border rounded w-fit py-1 px-2 mt-2 desktop:mt-0">
+                    <a
+                      href={speaker.pdf}
+                      target="_blank"
+                      className="text-mobile-body2 hover:text-gray-300 transition tablet:text-body3 flex items-center gap-1"
+                    >
+                      <SaveAltIcon className="w-4 h-4" /> 발표자료
+                    </a>
+                  </button>
                 </div>
                 <Image
                   src={speaker.src}
                   alt={speaker.id}
                   className={clsx(
-                    'tablet:w-[60px] tablet:h-[60px] desktop:w-[80px] desktop:h-[80px] desktop-xl:absolute desktop-xl:right-0 desktop-xl:bottom-1'
+                    'w-[50px] h-[50px] tablet:w-[60px] tablet:h-[60px] desktop:w-[80px] desktop:h-[80px]'
                   )}
-                  width={60}
-                  height={60}
                 />
               </div>
             </div>
