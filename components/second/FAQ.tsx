3588c3322da6a72ac2eaab86b9ae05486a71ff9b
cje <chej098t@gmail.com>
2023-10-19 02:25:04 +0900

    feat: 2회차 FAQ 내용 수정 및 트랜지션 추가 (#69)
    * feat: 2회차 FAQ 내용 수정
    
    * feat: FAQ 클릭 시 트랜지션 추가
    
    * fix(FAQ): second 폴더 내 FAQField 컴포넌트로 변경

--- components/second/FAQ.tsx
+++ components/second/FAQ.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import clsx from 'clsx'
 
-import FAQField from '@/components/first/common/FAQField'
+import FAQField from '@/components/second/common/FAQField'
 import { faqData } from '@/data/second/FAQ'
 import type { FAQ as FAQType } from '@/types'
 
@@ -30,7 +30,7 @@ const FAQ = () => {
             <button
               type="button"
               className={clsx(
-                'text-gray-900 px-2 py-1 tablet:px-3 tablet:py-2',
+                'text-gray-900 px-2 py-1 tablet:px-3 tablet:py-2 transition-all',
                 selected === Category.APPLY &&
                   'text-purple-main border-purple-main border-b'
               )}
@@ -41,7 +41,7 @@ const FAQ = () => {
             <button
               type="button"
               className={clsx(
-                'text-gray-90 px-2 py-1 tablet:px-3 tablet:py-2',
+                'text-gray-90 px-2 py-1 tablet:px-3 tablet:py-2 transition-all',
                 selected === Category.ON_SITE &&
                   'text-purple-main border-purple-main border-b'
               )}
@@ -52,7 +52,7 @@ const FAQ = () => {
             <button
               type="button"
               className={clsx(
-                'px-2 py-1 tablet:px-3 tablet:py-2 text-gray-900',
+                'px-2 py-1 tablet:px-3 tablet:py-2 text-gray-900 transition-all',
                 selected === Category.EVENT &&
                   'text-purple-main border-purple-main border-b'
               )}
@@ -63,7 +63,7 @@ const FAQ = () => {
             <button
               type="button"
               className={clsx(
-                'px-2 py-1 tablet:px-3 tablet:py-2 text-gray-900',
+                'px-2 py-1 tablet:px-3 tablet:py-2 text-gray-900 transition-all',
                 selected === Category.ETC &&
                   'text-purple-main border-purple-main border-b'
               )}
